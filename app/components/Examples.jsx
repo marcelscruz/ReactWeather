7d6e3ad8099672fc6909fe138df67b59b1c93d5d
Marcel Cruz <marcelcruz@live.com>
2017-09-02 11:47:24 +0100

    nav search support added

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -7,10 +7,10 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Dublin'>Dublin, Ireland</Link>
+          <Link to='/?location=Dublin, Ireland'>Dublin, Ireland</Link>
         </li>
         <li>
-          <Link to='/?location=Rio'>Rio, Brazil</Link>
+          <Link to='/?location=Rio, Brazil'>Rio, Brazil</Link>
         </li>
       </ol>
     </div>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -13,11 +13,16 @@ var Weather = React.createClass({
   handleSearch: function(location) {
     var that = this;
 
+    if (location === 'dublin' || location === 'Dublin') {
+      location = 'Dublin, Ireland';
+    }
+
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
-    // debugger;
 
     openWeatherMap.getTemp(location).then(function(temp) {
       that.setState({
@@ -32,6 +37,25 @@ var Weather = React.createClass({
       });
     });
   },
+
+  componentDidMount: function() {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+
   render: function() {
     var { isLoading, temp, location, errorMessage } = this.state;
 
