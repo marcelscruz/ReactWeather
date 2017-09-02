de925e16c55461efee1212eff2085aa12225dfb9
Marcel Cruz <marcelcruz@live.com>
2017-09-02 11:49:04 +0100

    nav search support added

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -13,6 +13,7 @@ var Weather = React.createClass({
   handleSearch: function(location) {
     var that = this;
 
+    // Redirect to Dublin, Ireland
     if (location === 'dublin' || location === 'Dublin') {
       location = 'Dublin, Ireland';
     }
